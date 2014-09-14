8ca36e2311d4e6f219608c8f453079c68620361f
ZhuGongpu <zhugongpu@hotmail.com>
2014-09-14 15:54:09 +0800

    添加折线图

--- HomeEnergyControl/src/enn/view/fragments/QueryFragment.java
+++ HomeEnergyControl/src/enn/view/fragments/QueryFragment.java
@@ -8,6 +8,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import enn.view.R;
+import org.eazegraph.lib.charts.ValueLineChart;
+import org.eazegraph.lib.models.ValueLinePoint;
+import org.eazegraph.lib.models.ValueLineSeries;
 
 
 /**
@@ -19,6 +22,43 @@ public class QueryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         Log.e(TAG, "onCreateView");
-        return inflater.inflate(R.layout.query_fragment, container, false);
+        View view = inflater.inflate(R.layout.query_fragment, container, false);
+        demo(view);
+        return view;
+    }
+
+    private void demo(View view) {
+        ValueLineChart mCubicValueLineChart = (ValueLineChart) view.findViewById(R.id.linechart);
+
+        ValueLineSeries series = new ValueLineSeries();
+        series.setColor(0xFF56B7F1);
+
+        series.addPoint(new ValueLinePoint("Jan", 2.4f));
+        series.addPoint(new ValueLinePoint("Feb", 3.4f));
+        series.addPoint(new ValueLinePoint("March", .4f));
+        series.addPoint(new ValueLinePoint("Apr", 1.2f));
+        series.addPoint(new ValueLinePoint("May", 2.6f));
+        series.addPoint(new ValueLinePoint("Jun", 1.0f));
+        series.addPoint(new ValueLinePoint("Jul", 3.5f));
+        series.addPoint(new ValueLinePoint("Aug", 2.4f));
+        series.addPoint(new ValueLinePoint("Sep", 2.4f));
+        series.addPoint(new ValueLinePoint("Oct", 3.4f));
+        series.addPoint(new ValueLinePoint("Nov", .4f));
+        series.addPoint(new ValueLinePoint("Dec", 1.3f));
+
+        series.addPoint(new ValueLinePoint("" + 0, 0f));
+        series.addPoint(new ValueLinePoint("" + 1, 1f));
+        series.addPoint(new ValueLinePoint("" + 2, 2f));
+        series.addPoint(new ValueLinePoint("" + 3, 3f));
+        series.addPoint(new ValueLinePoint("" + 4, 4f));
+        series.addPoint(new ValueLinePoint("" + 5, 5f));
+        series.addPoint(new ValueLinePoint("" + 6, 6f));
+        series.addPoint(new ValueLinePoint("" + 7, 7f));
+        series.addPoint(new ValueLinePoint("" + 8, 8f));
+        series.addPoint(new ValueLinePoint("" + 9, 9f));
+
+        mCubicValueLineChart.addSeries(series);
+
+        mCubicValueLineChart.startAnimation();
     }
 }
