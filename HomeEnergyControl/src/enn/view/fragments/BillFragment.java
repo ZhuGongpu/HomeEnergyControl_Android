5f6ec164f4002fbbab66161c50cbafbca29a7f1f
ZhuGongpu <zhugongpu@hotmail.com>
2014-09-20 14:08:03 +0800

    基本实现账单界面代码逻辑

--- HomeEnergyControl/src/enn/view/fragments/BillFragment.java
+++ HomeEnergyControl/src/enn/view/fragments/BillFragment.java
@@ -6,11 +6,16 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import enn.view.R;
 import org.eazegraph.lib.charts.StackedBarChart;
 import org.eazegraph.lib.communication.IOnBarClickedListener;
 import org.eazegraph.lib.models.BarModel;
 import org.eazegraph.lib.models.StackedBarModel;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 
 /**
@@ -19,10 +24,14 @@ import org.eazegraph.lib.models.StackedBarModel;
 public class BillFragment extends Fragment {
     private static final String TAG = "BillFragment";
 
+    private View view = null;
+
+    private List<StackedBarModel> models = new ArrayList<StackedBarModel>();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         Log.e(TAG, "onCreateView");
-        View view = inflater.inflate(R.layout.bill_fragment, container, false);
+        view = inflater.inflate(R.layout.bill_fragment, container, false);
 
         demo(view);
 
@@ -32,40 +41,139 @@ public class BillFragment extends Fragment {
     private void demo(View view) {
         StackedBarChart mStackedBarChart = (StackedBarChart) view.findViewById(R.id.stackedbarchart);
 
-        StackedBarModel s1 = new StackedBarModel("12.4");
+        models.add(generateStackedBarModel("01月"));
+        models.add(generateStackedBarModel("02月"));
+        models.add(generateStackedBarModel("03月"));
+        models.add(generateStackedBarModel("04月"));
+        models.add(generateStackedBarModel("05月"));
+        models.add(generateStackedBarModel("06月"));
 
-        s1.addBar(new BarModel(1f, 0xFF63CBB0));
-        s1.addBar(new BarModel(1f, 0xFF56B7F1));
-        s1.addBar(new BarModel(1f, 0xFFCDA67F));
+        mStackedBarChart.addBarList(models);
 
-        StackedBarModel s2 = new StackedBarModel("13.4");
-        s2.addBar(new BarModel(1f, 0xFF63CBB0));
-        s2.addBar(new BarModel(2f, 0xFF56B7F1));
-        s2.addBar(new BarModel(3f, 0xFFCDA67F));
-
-        StackedBarModel s3 = new StackedBarModel("14.4");
-
-        s3.addBar(new BarModel(2f, 0xFF63CBB0));
-        s3.addBar(new BarModel(3f, 0xFF56B7F1));
-        s3.addBar(new BarModel(4f, 0xFFCDA67F));
-
-        StackedBarModel s4 = new StackedBarModel("15.4");
-        s4.addBar(new BarModel(1f, 0xFF63CBB0));
-        s4.addBar(new BarModel(2f, 0xFF56B7F1));
-        s4.addBar(new BarModel(3f, 0xFFCDA67F));
-
-        mStackedBarChart.addBar(s1);
-        mStackedBarChart.addBar(s2);
-        mStackedBarChart.addBar(s3);
-        mStackedBarChart.addBar(s4);
+        setBarSelected(models.size() - 1);
+        setYear(Calendar.getInstance().get(Calendar.YEAR) + "年");
 
         mStackedBarChart.startAnimation();
-
         mStackedBarChart.setOnBarClickedListener(new IOnBarClickedListener() {
             @Override
             public void onBarClicked(int _Position) {
                 Log.e(TAG, "onBarClicked : " + _Position);
+                //响应点击事件 : 切换下部的标签内容
+                setBarSelected(_Position);
             }
         });
     }
+
+
+    /**
+     * 根据选中的StackBar设置各种标签
+     *
+     * @param position
+     */
+    private void setBarSelected(int position) {
+        StackedBarModel model = models.get(position);
+        List<BarModel> barModels = model.getBars();
+        //设置月份
+        setMonth(model.getLegendLabel());
+        //设置总费用
+        setSumCost(model.getSumValue() + "");
+
+        //TODO 向服务器请求或本地计算usage
+
+        //设置水相关的数据
+        setWaterFee(barModels.get(0).getValue() + "");
+
+        //设置热相关的数据
+        setHeatFee(barModels.get(1).getValue() + "");
+
+        //设置电相关的数据
+        setElectricityFee(barModels.get(2).getValue() + "");
+
+        //设置气相关的数据
+        setGasFee(barModels.get(3).getValue() + "");
+
+    }
+
+
+    /**
+     * 生成StackedBarModel
+     * dummy implementation
+     *
+     * @param name
+     * @return
+     */
+    private StackedBarModel generateStackedBarModel(String name) {
+        StackedBarModel model = new StackedBarModel(name);
+
+        model.addBar(new BarModel(1f, getResources().getColor(R.color.green)));
+        model.addBar(new BarModel(1f, getResources().getColor(R.color.red)));
+        model.addBar(new BarModel(1f, getResources().getColor(R.color.blue)));
+        model.addBar(new BarModel(1f, getResources().getColor(R.color.yellow)));
+
+        return model;
+    }
+
+    private void setYear(String year) {
+        if (this.view != null) {
+            ((TextView) (this.view.findViewById(R.id.bill_fragment_year))).setText(year);
+        }
+    }
+
+    private void setMonth(String month) {
+        if (this.view != null) {
+            ((TextView) (this.view.findViewById(R.id.bill_fragment_month))).setText(month);
+        }
+    }
+
+    private void setSumCost(String sumCost) {
+        if (this.view != null) {
+            ((TextView) (this.view.findViewById(R.id.bill_fragment_sum_cost))).setText(sumCost);
+        }
+    }
+
+    private void setGasUsage(String gasUsage) {
+        if (this.view != null) {
+            ((TextView) (this.view.findViewById(R.id.bill_fragment_gas_usage))).setText(gasUsage);
+        }
+    }
+
+    private void setGasFee(String gasFee) {
+        if (this.view != null) {
+            ((TextView) (this.view.findViewById(R.id.bill_fragment_gas_money))).setText(gasFee);
+        }
+    }
+
+    private void setElectricityUsage(String electricityUsage) {
+        if (this.view != null) {
+            ((TextView) (this.view.findViewById(R.id.bill_fragment_electricity_usage))).setText(electricityUsage);
+        }
+    }
+
+    private void setElectricityFee(String electricityFee) {
+        if (this.view != null) {
+            ((TextView) (this.view.findViewById(R.id.bill_fragment_electricity_money))).setText(electricityFee);
+        }
+    }
+
+    private void setHeatUsage(String heatUsage) {
+        if (this.view != null)
+            ((TextView) (this.view.findViewById(R.id.bill_fragment_heat_usage))).setText(heatUsage);
+
+    }
+
+    private void setHeatFee(String heatFee) {
+        if (this.view != null)
+            ((TextView) (this.view.findViewById(R.id.bill_fragment_heat_money))).setText(heatFee);
+    }
+
+    private void setWaterUsage(String waterUsage) {
+        if (this.view != null)
+            ((TextView) (this.view.findViewById(R.id.bill_fragment_water_usage))).setText(waterUsage);
+    }
+
+    private void setWaterFee(String waterFee) {
+        if (this.view != null)
+            ((TextView) (this.view.findViewById(R.id.bill_fragment_water_money))).setText(waterFee);
+    }
+
 }
